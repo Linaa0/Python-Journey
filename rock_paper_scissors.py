9c58806975f527cd7a5ed146d5334a5e66160134
Linaa <ishimwelina06@gmail.com>
2026-07-26 22:37:33 +0200

    Third Project

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,10 @@
+import random 
+
+emojis={'r': '🪨', 's': '✂️', 'p':'📜'}
+choices= ('r','p','s')
 choice= input('Rock, Paper, Scissors (r/p/s): ').lower()
-if choice !='r' and choice != 'p' and choice != 's':
-    print('Invalid choice')
+if choice not in choices:
+    print('Invalid choice!')
+computer_choice= random.choice(choices)    
+print(f'You chose {emojis[choice]}')
+print(f'The computer chose {emojis[computer_choice]}')
