93038a8e0ea2e0d4265bdc03bdf57191a33112ad
Linaa <ishimwelina06@gmail.com>
2026-08-01 00:09:54 +0200

    Third Project

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,18 +6,16 @@ choices= ('r','p','s')
 def get_user_choice():
   while True:
     choice= input('Rock, Paper, Scissors (r/p/s): ').lower()
-    if choice not in choices:
-     print('Invalid choice!')
-    continue
-while True:
-   
-   
+    if choice in choices:
+     return choice
+    else:
+     print('Invalid Choice!')
 
-    computer_choice= random.choice(choices)    
+def display_choices(choice, computer_choice):
+   print(f'You chose {emojis[choice]}')
+   print(f'The computer chose {emojis[computer_choice]}')
 
-    print(f'You chose {emojis[choice]}')
-    print(f'The computer chose {emojis[computer_choice]}')
-
+def determine_the_winner(choice, computer_choice):
     if choice == computer_choice:
         print('Tie!')
     elif (
@@ -28,6 +26,12 @@ while True:
     else :
         print('You lose!')   
 
-    want_continue= input('Continue? (y/n): ') .lower() 
-    if want_continue=='n':
-        break   
+    def play_game():
+      while True:
+        choice= get_user_choice()
+        computer_choice= random.choice(choices)  
+        display_choices(choice, computer_choice)
+        determine_the_winner(choice, computer_choice)
+        want_continue= input('Continue? (y/n): ') .lower() 
+        if want_continue=='n':
+             break   
