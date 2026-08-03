62571d73c2c21163110f4aff7b76d276b446ff0b
Linaa <ishimwelina06@gmail.com>
2026-08-03 08:38:48 +0200

    AI First Project

--- AI_Project_One/main.py
+++ AI_Project_One/main.py
@@ -2,7 +2,7 @@
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain.tools import tool
-from langgraph.prebuilt import create_agent
+from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,7 +10,7 @@ load_dotenv()
 def main():
     model=ChatOpenAI(temperature=0)
     tools= []
-    agent_executor= create_agent(model, tools)
+    agent_executor= create_react_agent(model, tools)
 
     print("Welcome! I am your AI assistant, Type 'quit' to exist.")
     print("You can ask me to perform calculations or chat with me.")
