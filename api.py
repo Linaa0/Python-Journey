e65191ebbf395eea8ffc60281fff7a7837b03812
Linaa <ishimwelina06@gmail.com>
2026-08-07 10:57:27 +0200

    API Handling

--- api.py
+++ api.py
@@ -1,2 +1,29 @@
 import requests
 import json 
+from IPython.display import Markdown, display
+
+question="What is the weather today in Kigali?"
+
+url= "https://api.ejplabs.com/api/v1/subiza"
+
+headers= {
+   "X-API-Key": ""
+ }
+
+data= {
+    "messages":[
+        {
+          "role":"user",
+          "content": question
+       } 
+
+    ]
+}
+
+response= requests.post(
+    url,
+    headers=headers,
+    json=data
+)
+
+print(response.json())
